e65418e5f88fbeb803806cf6b1afd77b70689945
Breno Machado do Monte <brenomachadodomonte@gmail.com>
2021-09-13 11:46:13 -0300

    Creating css styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="calc">
     <h1>Vue Calculator</h1>
   </div>
 </template>
@@ -11,5 +11,29 @@ export default {
 </script>
 
 <style>
+  @font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+  }
+
+  * {
+    font-family: "RobotoMono", monospace;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  #app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+  }
 
 </style>
